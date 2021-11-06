74f4aa67c764e5702dde7b264b284fba22229542
天不生我 <2082547120@qq.com>
2021-11-06 14:13:51 +0800

    初始化

---
+++ src/main/java/com/byteDance/GrayRelease/infrastructure/pojo/RuleDO.java
@@ -0,0 +1,12 @@
+package com.byteDance.GrayRelease.infrastructure.pojo;
+
+
+import com.byteDance.GrayRelease.common.BaseRule;
+
+/**
+ * 数据库DO对象
+ * @auther: cwj
+ */
+public class RuleDO extends BaseRule {
+
+}
